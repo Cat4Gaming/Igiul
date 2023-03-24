428824d09f54cdaa5023f5314fc1b1bd1f9c1547
Catz-Gamez <andreas.moosbauer3118@gmail.com>
2023-03-24 09:43:34 +0100

    FullScreen + Scale

--- igiul/src/main/java/com/igiul/Game.java
+++ igiul/src/main/java/com/igiul/Game.java
@@ -1,18 +1,29 @@
 package com.igiul;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
 public class Game extends Application{
     private Stage stage;
-    private Label label;
+    private Label title, subtitle;
     private Button button;
+    private static double scaleX, scaleY;
     public static void main(String[] args){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        scaleX = screenSize.getWidth()/32;
+        scaleY = screenSize.getHeight()/18;
         launch(args);
     }
     @Override
@@ -20,23 +31,30 @@ public class Game extends Application{
         stage = ps;
         stage.setTitle("Igiul - Gioco-d'Azzardo");
         stage.setResizable(false);
-        label = new Label("Start");
+        title = new Label("Igiul");
+        title.setFont(new Font(40));
+        title.setAlignment(Pos.TOP_CENTER);
+        subtitle = new Label("Gioco-d'Azzardo");
         button = new Button();
         button.setText("joa");
+        button.setPrefSize(1*scaleX, 1*scaleY);
         button.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent e){
-                label.setText("Starte");
                 button.setText("thx");
                 button.setDisable(true);
             }
         });
         VBox box = new VBox();
-        box.getChildren().add(label);
+        box.getChildren().add(title);
+        box.getChildren().add(subtitle);
         box.getChildren().add(button);
-        Scene scene0 = new Scene(box, 300, 250);
+        Scene scene0 = new Scene(box);
+        scene0.setFill(null);
         ps.setScene(scene0);
         ps.setFullScreen(true);
+        ps.setFullScreenExitHint("");
+        ps.setFullScreenExitKeyCombination(KeyCombination.valueOf("Ü"));
         ps.show();
     }
 }
--- igiul/src/main/java/module-info.java
+++ igiul/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.igiul {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens com.igiul to javafx.fxml;
     exports com.igiul;
