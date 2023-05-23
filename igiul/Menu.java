08889cf72eb9d594a460d188477b82f2997da579
Cat4Gaming <102665654+Cat4Gaming@users.noreply.github.com>
2023-05-23 08:06:50 +0200

    Menu Update

--- igiul/Menu.java
+++ igiul/Menu.java
@@ -17,7 +17,7 @@ public class Menu extends JPanel {
     
     private void createGUI() {
         try {
-            font = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/Darumadrop_One/DarumadropOne-Regular.ttf")).deriveFont(32f);
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/Darumadrop_One/DarumadropOne-Regular.ttf")).deriveFont(128f);
         } catch(IOException| FontFormatException e) {}
         setBounds(0, 0, screenWidth, screenHeight);
         setLayout(new BorderLayout());
@@ -30,10 +30,21 @@ public class Menu extends JPanel {
         JLabel titleText = new JLabel("Igiul", SwingConstants.CENTER);
         titleText.setFont(font);
         titleText.setForeground(Color.WHITE); 
-
+        titleText.setBorder(BorderFactory.createEmptyBorder(5, 5, 200, 5));
+        
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("assets/fonts/Darumadrop_One/DarumadropOne-Regular.ttf")).deriveFont(32f);
+        } catch(IOException| FontFormatException e) {}
         JButton startButton = new JButton("Start Game");
         startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         startButton.setFocusable(false);
+        startButton.setIcon(owner.resizedImageIcon("assets/gfx/middlebutton.png", 200, 50));
+        startButton.setBorder(BorderFactory.createEmptyBorder());
+        startButton.setVerticalTextPosition(SwingConstants.CENTER);
+        startButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        startButton.setFont(font);
+        startButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        startButton.setContentAreaFilled(false);
         startButton.addActionListener(event -> {
             SwingUtilities.invokeLater(() -> owner.showView(new PicturePoker(owner)));
         });
@@ -41,6 +52,13 @@ public class Menu extends JPanel {
         JButton resetButton = new JButton("Reset Game");
         resetButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         resetButton.setFocusable(false);
+        resetButton.setIcon(owner.resizedImageIcon("assets/gfx/middlebutton.png", 200, 50));
+        resetButton.setBorder(BorderFactory.createEmptyBorder());
+        resetButton.setVerticalTextPosition(SwingConstants.CENTER);
+        resetButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        resetButton.setFont(font);
+        resetButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        resetButton.setContentAreaFilled(false);
         resetButton.addActionListener(event -> {
             owner.deleteFile("saves/PicturePoker/save.dat");
         });
@@ -48,6 +66,13 @@ public class Menu extends JPanel {
         JButton closeButton = new JButton("Close Game");
         closeButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         closeButton.setFocusable(false);
+        closeButton.setIcon(owner.resizedImageIcon("assets/gfx/middlebutton.png", 200, 50));
+        closeButton.setBorder(BorderFactory.createEmptyBorder());
+        closeButton.setVerticalTextPosition(SwingConstants.CENTER);
+        closeButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        closeButton.setFont(font);
+        closeButton.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        closeButton.setContentAreaFilled(false);
         closeButton.addActionListener(event -> {
             owner.dispose();
         });
